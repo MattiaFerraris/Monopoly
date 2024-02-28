9f011fe732e8c9962b2a6d1a4a401071c2f29cd8
jakubbocian <79313373+jakubbocian@users.noreply.github.com>
2024-02-28 13:29:40 +0100

    Implementazione classi: Bank e Dice.

--- src/gameLogic/Dice.java
+++ src/gameLogic/Dice.java
@@ -1,4 +1,16 @@
 package gameLogic;
 
+import java.util.Random;
+
 public class Dice {
+    private final int faces;
+
+    public Dice(int faces) {
+        this.faces = faces;
+    }
+
+    public int roll(){
+        Random random = new Random();
+        return random.nextInt(faces) + 1;
+    }
 }
