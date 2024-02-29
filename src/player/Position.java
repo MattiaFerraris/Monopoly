7ba991baf43f10840799e58545eb63b9f6d1b531
mattia <mattia.ferraris@supsi.ch>
2024-02-29 08:24:03 +0100

    Metodi e parametri di Player e Position

--- src/player/Position.java
+++ src/player/Position.java
@@ -1,4 +1,14 @@
 package player;
 
 public class Position {
+    private int positionNumber = 0;
+
+    void updatePosition(int dice){
+        positionNumber = positionNumber + dice;
+
+    }
+
+    public int getPosition() {
+        return positionNumber;
+    }
 }
