a23017eabab6961e8d15b95449ef450d03782228
jakubbocian <79313373+jakubbocian@users.noreply.github.com>
2024-03-05 14:36:48 +0100

    Inizio implementazione ScannerUtilities.

---
+++ src/utility/ScannerUtilities.java
@@ -0,0 +1,20 @@
+package utility;
+
+import java.util.Scanner;
+
+public class ScannerUtilities {
+    private final Scanner scanner;
+
+    public ScannerUtilities() {
+        scanner = new Scanner(System.in);
+    }
+
+    public int readInt(String message) {
+        System.out.print(message);
+        while (!scanner.hasNextInt()) {
+            System.out.println(message);
+            scanner.next();
+        }
+        return scanner.nextInt();
+    }
+}
