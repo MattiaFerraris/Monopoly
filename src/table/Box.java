eaf366e1aa602992facc2a81b6ebaad236e7966f
jakubbocian <79313373+jakubbocian@users.noreply.github.com>
2024-03-26 18:03:25 +0100

    Revert

--- src/table/Box.java
+++ src/table/Box.java
@@ -2,40 +2,66 @@ package table;
 
 import game.Game;
 import player.Player;
-
-import java.util.Arrays;
 import java.util.Random;
 
 
-public abstract class Box {
-    protected Colors color;
-    protected int money;
-    protected String name;
+public class Box {
+    Types type;
+    private int money;
+    private int index;
     final static int boxWidth = 24;
     final static int height = 5;
 
-    private static final int minMoney = 50;
-    private static final int maxMoney = 150;
-    protected Player[] playersInTheBox;
+    private String[] boxDetails;
+    public Player[] playersInTheBox;
 
-    Box(Colors color, int money, String name) {
-        this.color = color;
+    Box(Types type, int money, int index) {
+        this.type = type;
         this.money = money;
-        this.name = name;
+        this.index = index;
         this.playersInTheBox = new Player[Game.NUMBER_OF_PLAYERS];
+        this.boxDetails = makeDetailsString(type, money, playersInTheBox);
 
         //creo un'array di stringhe contenente i dettagli
         //di ogni box sotto forma di stringhe (le dimensioni sono impostate manualmente).
     }
 
-    Box(Colors color, String name){
-        this(color, generateMoneyValue(minMoney, maxMoney) ,name);
-    }
-
-    static int generateMoneyValue(int min, int max) {
+    static int generateMoneyValue(int min, int max, int index) {
+        if (index == 0) {
+            return 100;
+        }
         Random random = new Random();
         return random.nextInt(max - min + 1) + min;
     }
+
+    static String getTypeString(Types type) {
+        return type.toString();
+    }
+
+    static String getMoneyString(Types type, int money) {
+        Types[] arrTypes = Types.values();
+
+        if (type.toString().equalsIgnoreCase("start")) { //se è lo start
+            return "Get: " + Integer.toString(money);
+        }
+        if (type.toString().equalsIgnoreCase("toll")) { //se è un pedaggio
+            return "Pay: " + Integer.toString(money);
+        }
+        return "";
+
+    }
+
+    static Types picType(int index) {
+        if (index == 0) {
+            return Types.START;
+        }
+        return Types.TOLL;
+    }
+
+    String[] makeDetailsString(Types type, int money, Player[] players) {
+        return new String[]{getTypeString(type), getMoneyString(type, money), "", "", playersToString()};
+    }
+
 
     public void removePlayerFromTheBox(Player player) {
         for (int i = 0; i < playersInTheBox.length; i++) {
@@ -55,12 +81,28 @@ public abstract class Box {
         }
     }
 
-    public int getMoney() {
-        return money;
+    String playersToString() {
+        String tmp = "";
+        for (Player inTheBox : playersInTheBox) {
+            if (inTheBox != null) {
+                tmp += inTheBox.getSymbol() + " ";
+            }
+        }
+        return tmp;
     }
 
-    @Override
-    public String toString() {
-        return name + ",";
+    public int getMoney(){
+        return money;
+    }
+    public int getIndex(){
+        return index;
+    }
+
+    public String[] getBoxDetails(){
+        return boxDetails;
+    }
+
+    public void setBoxDetails(String[] boxDetails) {
+        this.boxDetails = boxDetails;
     }
 }
